380f1c7fdb560c2b18ce960de2a00d77d9a021c5
Franco <francorsr98@gmail.com>
2021-03-05 22:55:43 -0600

    Added four different cards, to be able to reuse them in the project

--- src/pages/Followings.js
+++ src/pages/Followings.js
@@ -1,5 +1,20 @@
+import NavBar from "../components/containers/NavBar";
+import Pagination from "../components/containers/Pagination";
+import Card from '../components/containers/Card';
+import Avatar from '../components/UI/Avatar';
+import { Content } from '../components/text/Content';
+
 function Followings() {
-    return <div></div>
+    return (
+        <section>
+            <Pagination />
+            <Card type="follow">
+                <Avatar src="" measure="40" />
+                <Content>LazyBwoy</Content>
+            </Card>
+            <NavBar />
+        </section>
+    )
 }
 
 export default Followings;
